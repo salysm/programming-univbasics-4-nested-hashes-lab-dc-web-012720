885deb127c3cca249c2e524b41a0b2b74d45a316
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 09:12:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,13 +62,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  programmer_hash = [:yukihiro_matsumoto]
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -84,6 +77,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:first] = 
 
     
 end

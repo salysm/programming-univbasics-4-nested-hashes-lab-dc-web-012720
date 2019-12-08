fb0c7b6675c6197647d2b75ef52375c85a23f5b0
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 09:26:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash
 
 end
